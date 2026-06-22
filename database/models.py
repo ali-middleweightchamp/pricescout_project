e84e5357a7dc8afc8d02b0f91ce1a0bef4b733da
ali-middleweightchamp <aliyuuuyu@gmail.com>
2026-06-22 14:41:32 +0500

    feat: implement save_product and verify DB integration

--- database/models.py
+++ database/models.py
@@ -1,4 +1,5 @@
 from database.connection import get_db_connection
+from core.base_parser import Product
 import asyncio
 
 async def create_tables():
@@ -14,12 +15,44 @@ async def create_tables():
     async with get_db_connection() as conn:
         await conn.execute(sql)
         await conn.commit()
+
+
             
+async def save_product(product: Product):
+    sql = "INSERT INTO products (name, price, category) VALUES (?, ?, ?);"
+    async with get_db_connection() as conn:
+        await conn.execute(sql, (product.name, product.price, product.category))
+        await conn.commit()
+
        
 
 if __name__ == "__main__":
     import asyncio
     
-    print("🛠️ Инициализация базы данных...")
-    asyncio.run(create_tables())
-    print("✨ Таблицы успешно созданы!")
+    async def main_test():
+        print("🛠️ Инициализация базы данных...")
+        await create_tables()
+        print("✨ Таблицы готовы!")
+        
+        # Создаем тестовый объект товара
+        test_item = Product(name="iPhone 15 Pro Max", price=16000000.0, category="Смартфоны")
+        
+        print(f"📥 Пробуем сохранить: {test_item.name}...")
+        asyncio.sleep(3)
+        await save_product(test_item)
+        print("✅ Товар успешно сохранен в SQL!")
+        
+        # Проверим, что он реально там лежит
+        print("\n🔎 Проверяем содержимое базы данных:")
+        async with get_db_connection() as conn:
+            async with conn.execute("SELECT * FROM products;") as cursor:
+                rows = await cursor.fetchall()
+                for row in rows:
+                    # Помнишь row_factory? Достаем данные по именам колонок!
+                    print(f"ID: {row['id']} | Товар: {row['name']} | Цена: {row['price']} сум | Дата: {row['scraped_at']}")
+
+    asyncio.run(main_test())
+
+
+
+    
